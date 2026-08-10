26ebcffadf72542f0dbb83e8f3aa57057fe7fdb6
samadfastnexa <gpo.ai.fastnexa@gmail.com>
2026-08-10 13:47:42 +0500

    fix: rename FieldAdvisoryService admin label to Farmer Meeting

--- web_portal/FieldAdvisoryService/apps.py
+++ web_portal/FieldAdvisoryService/apps.py
@@ -4,6 +4,7 @@ from django.apps import AppConfig
 class FieldadvisoryserviceConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'FieldAdvisoryService'
+    verbose_name = 'Farmer Meeting'
     
     def ready(self):
         import FieldAdvisoryService.signals  # noqa: F401
